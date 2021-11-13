1716200698e0dbd5b6299cc480706cd13cc91d52
王雪 <broken.ice1314@gmail.com>
2021-11-13 08:43:44 +0800

    fix index image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Header from '../components/header'
 import ExtLink from '../components/ext-link'
 import Features from '../components/features'
+import Image from 'next/image'
 import sharedStyles from '../styles/shared.module.css'
 
 export default function Index() {
@@ -8,12 +9,15 @@ export default function Index() {
     <>
       <Header titlePre="Home" />
       <div className={sharedStyles.layout}>
-        <img
-          src="/goooice-and-notion.png"
-          height="85"
-          width="250"
-          alt="Goooice + Notion"
-        />
+        <section>
+          <Image
+            src="/goooice-and-notion.png"
+            layout="fixed"
+            height="85"
+            width="250"
+            alt="Goooice + Notion"
+          />
+        </section>
         <h1>欢迎访问GoooIce的MiantuNet</h1>
         <h2>又一个记录与分享的博客 </h2>
 
