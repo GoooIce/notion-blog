72664bd0fc0856607dc681c968d0ee4ef74b1a5c
王雪 <broken.ice1314@gmail.com>
2024-03-04 01:56:31 +0800

    update: image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Header from '../components/header'
 import ExtLink from '../components/ext-link'
 import Features from '../components/features'
-import Image from 'next/image'
+import Image from 'next/legacy/image'
 import sharedStyles from '../styles/shared.module.css'
 
 export default function Index() {
